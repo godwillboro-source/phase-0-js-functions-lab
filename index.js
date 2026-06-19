66734dd7e7a7afa7d3b5d6e0644a9e2dcbd69247
godwillboro-source <godwillboro@gmail.com>
2026-06-19 08:28:03 +0300

    first commit

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
+function calculateTax(amount) {
+  let tax = amount * 0.10;
+  return tax;
+}
+let amount = 50000;
+let tax = calculateTax(amount);
+console.log(tax);
 
+function convertToUpperCase(text) {
+  return text.toUpperCase();
+}
+let text = "hello, world!";
+console.log(convertToUpperCase(text));
 
+function isPalindrome(text1) {
+    let reverse = text1.split('').reverse().join('');
+    return text1 === reverse;
+}
+let text1 = "Racecar";
+console.log(isPalindrome(text1));
 
-
-
-// This is required for the test to function properly  
-module.exports = { calculateTax, convertToUpperCase, findMaximum, isPalindrome, calculateDiscountedPrice };
+function calculateDiscountedPrice(originalPrice, discountPercentage) {
+  const discountAmount = originalPrice * (discountPercentage / 100);
+  return originalPrice - discountAmount;
+}
+let originalPrice = 100;
+let discountPercentage = 20;
+let discountedPrice = calculateDiscountedPrice(originalPrice, discountPercentage);
+console.log(discountedPrice);
