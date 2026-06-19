cea4499ba422fc83c013ff2030a7a4bd6120a56e
godwillboro-source <godwillboro@gmail.com>
2026-06-19 09:18:38 +0300

    final changes

--- index.js
+++ index.js
@@ -2,28 +2,25 @@ function calculateTax(amount) {
   let tax = amount * 0.10;
   return tax;
 }
-let amount = 50000;
-let tax = calculateTax(amount);
-console.log(tax);
-
 function convertToUpperCase(text) {
   return text.toUpperCase();
 }
-let text = "hello, world!";
-console.log(convertToUpperCase(text));
-
-function isPalindrome(text1) {
-    let reverse = text1.split('').reverse().join('');
-    return text1 === reverse;
+function findMaximum(num1, num2) {
+  return num1 > num2 ? num1 : num2;
 }
-let text1 = "Racecar";
-console.log(isPalindrome(text1));
-
+function isPalindrome(word) {
+    let reverse = word.split('').reverse().join('');
+    return word === reverse;
+}
 function calculateDiscountedPrice(originalPrice, discountPercentage) {
   const discountAmount = originalPrice * (discountPercentage / 100);
   return originalPrice - discountAmount;
 }
-let originalPrice = 100;
-let discountPercentage = 20;
-let discountedPrice = calculateDiscountedPrice(originalPrice, discountPercentage);
-console.log(discountedPrice);
+
+module.exports = {
+  calculateTax,
+  convertToUpperCase,
+  findMaximum,
+  isPalindrome,
+  calculateDiscountedPrice,
+};
